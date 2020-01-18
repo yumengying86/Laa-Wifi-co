4f9f4243bfe8da013e736f8fe761938f19dd47cb
HaoYin <mauri@foxmail.com>
2020-01-18 09:37:32 +0800

    modify 1v1.sh

--- 1v1.sh
+++ 1v1.sh
@@ -1,71 +1,64 @@
 #! /bin/bash
 
+# lte only
+mkdir lte_only
+taskset -c 1 nohup ./waf --run "scratch/laa --numA=1 --numB=1 --d2=10 --cellConfigA=Lte --shutB=true --phyLogName=_phy_log_lte_only --udpRate=750000000bps --duration=120 --logPhyArrivals=true" 2>&1  > ./lte_only/lte &
 
-# test
-## sed -i "446c dcf->SetTxopLimit (MicroSeconds (3008));"  src/wifi/model/wifi-mac.cc
-# ./waf --run "scratch/laa --numA=1 --numB=1 --duration=30 --lbtTxop=3 --logPhyArrivals=true"
 
-# # laa and wifi coexistence, 1v1
-# # Change d2 from 10 to 50m
-# int=1
-# while(( $int<=5 ))
-# do
-#     taskset -c $int nohup ./waf --run "scratch/laa --numA=1 --numB=1 --d2=`expr $int \* 10` --phyLogName=_phy_log_$int --duration=1 --logPhyArrivals=true" 2>&1  > ../distance/d$int &
-#     sleep 1
-#     let "int++"
-# done
+# laa only and wifi only
+# neither laa nor wifi affect each other 
+mkdir lte_wifi_only
+taskset -c 2 nohup ./waf --run "scratch/laa --numA=1 --numB=1 --d2=500 --phyLogName=_phy_log_lte_wifi_only --udpRate=750000000bps --duration=120 --logPhyArrivals=true" 2>&1  > ./lte_wifi_only/lte_wifi &
 
-# # lte only
-# ./waf --run "scratch/test --numA=1 --numB=1 --duration=30 --udpRate=300000000 --shutB=true --cellConfigA=Lte --d2=500 --logPhyArrivals=true" 
-
-# # laa only and wifi only
-# # neither laa nor wifi affect each other 
-# # txop=8
-# sed -i "446c       dcf->SetTxopLimit (MicroSeconds (8000));"  src/wifi/model/wifi-mac.cc
-# ./waf --run "scratch/test --numA=1 --numB=1 --duration=30 --udpRate=300000000 --d2=500 --logPhyArrivals=true" 
-
-# # laa only and wifi only
-# # neither laa nor wifi affect each other 
-# # txop=3
-# sed -i "446c       dcf->SetTxopLimit (MicroSeconds (3008));"  src/wifi/model/wifi-mac.cc
-# ./waf --run "scratch/test --numA=1 --numB=1 --duration=30 --udpRate=300000000 --d2=500 --lbtTxop=3 --logPhyArrivals=true" 
 
 # laa and wifi coexistence, 1v1
 # Change laa's txop from 1 to 8ms
 int=1
-mkdir ../10m_15s_1v1_lbttxop
+mkdir 10m_120s_1v1_lbttxop
 while(( $int<=8 ))
 do
-    taskset -c $int nohup ./waf --run "scratch/laa --numA=1 --numB=1 --d2=10 --lbtTxop=${int} --phyLogName=_phy_log_10m_$int --duration=15 --logPhyArrivals=true" 2>&1  > ../10m_15s_1v1_lbttxop/lbttxop$int &
+    core_name=`expr $int \+ 2`
+    taskset -c $core_name nohup ./waf --run "scratch/laa --numA=1 --numB=1 --d2=10 --lbtTxop=$int --phyLogName=_phy_log_1v1_lbttxop_$int --udpRate=750000000bps --duration=120 --logPhyArrivals=true" 2>&1  > ./10m_120s_1v1_lbttxop/lbttxop$int &
     sleep 1
     let "int++"
 done
 
-# # laa and wifi coexistence, 1v1
-# # Change wifi's txop from 1 to 8ms
-# for int in 992 2016 3008 4000 4992 6016 7008 8000
-# do	
-# 	sed -i "446c       dcf->SetTxopLimit (MicroSeconds (${int}));"  src/wifi/model/wifi-mac.cc
-# 	./waf --run "scratch/test --numA=1 --numB=1 --duration=30 --udpRate=300000000 --lbtTxop=3 --logPhyArrivals=true"
-#     mv laa_wifi_simple_default_phy_log wifi1v1txop${int}.txt
-# done	
 
-# # laa and wifi coexistence, 3v3
-# # Change laa's txop from 1 to 8ms
-# int=1
-# while(( $int<=8 ))
-# do
-#     sed -i "446c       dcf->SetTxopLimit (MicroSeconds (3008));"  src/wifi/model/wifi-mac.cc
-#     ./waf --run "scratch/test --numA=3 --numB=3 --duration=30 --udpRate=300000000 --lbtTxop=${int} --logPhyArrivals=true"  
-#     mv laa_wifi_simple_default_phy_log laa3v3txop${int}.txt
-#     let "int++"
-# done
+# laa and wifi coexistence, 1v1, lbttxop=5
+# Change laa's defer X from 1 to 10
+int=1
+mkdir 10m_120s_1v1_lbt_X
+while(( $int<=10 ))
+do
+    core_name=`expr $int \+ 10`
+    taskset -c $core_name nohup ./waf --run "scratch/laa --numA=1 --numB=1 --d2=10 --lbtTxop=5 --laaDeferX=$int --phyLogName=_phy_log_1v1_lbt_X_$int --udpRate=750000000bps --duration=120 --logPhyArrivals=true" 2>&1  > ./10m_120s_1v1_lbt_X/X$int &
+    sleep 1
+    let "int++"
+done
 
-# # laa and wifi coexistence, 3v3
-# # Change wifi's txop from 1 to 8ms
-# for int in 992 2016 3008 4000 4992 6016 7008 8000
-# do	
-# 	sed -i "446c       dcf->SetTxopLimit (MicroSeconds (${int}));"  src/wifi/model/wifi-mac.cc
-# 	./waf --run "scratch/test --numA=3 --numB=3 --duration=30 --udpRate=300000000 --lbtTxop=3 --logPhyArrivals=true"
-#     mv laa_wifi_simple_default_phy_log wifi3v3txop${int}.txt
-# done
+# laa and wifi coexistence, 1v1
+# Change laa's cwmin from 0 to 1023
+int=0
+mkdir 10m_120s_1v1_lbt_cwmin
+while(( $int<=10 ))
+do
+    core_name=`expr $int \+ 21`
+    let "l=2**$int"
+    l=`expr $l \- 1`
+    taskset -c $core_name nohup ./waf --run "scratch/laa --numA=1 --numB=1 --d2=10 --lbtTxop=5 --laaMinCw=$l --laaMaxM=`expr 10 \- $int` --phyLogName=_phy_log_1v1_lbt_cwmin_$l --udpRate=750000000bps --duration=120 --logPhyArrivals=true" 2>&1  > ./10m_120s_1v1_lbt_cwmin/cwmin$int &
+    sleep 1
+    let "int++"
+done
+
+
+# laa and wifi coexistence, 1v1
+# Change laa's cwmax from 0 to 1023
+int=1
+mkdir 10m_120s_1v1_lbt_cwmax_M
+while(( $int<=6 ))
+do
+    core_name=`expr $int \+ 31`
+    taskset -c $core_name nohup ./waf --run "scratch/laa --numA=1 --numB=1 --d2=10 --lbtTxop=5 --laaMaxM=$int --phyLogName=_phy_log_1v1_lbt_cwmax__M$int --udpRate=750000000bps --duration=120 --logPhyArrivals=true" 2>&1  > ./10m_120s_1v1_lbt_cwmax_M/M$int &
+    sleep 1
+    let "int++"
+done
