2bc17b40959065e0b35035cf7b0eed1bdcb7dfc5
HaoYin <mauri@foxmail.com>
2020-02-05 10:50:15 +0800

    modify nvn.sh

--- nvn.sh
+++ nvn.sh
@@ -5,8 +5,8 @@ int=2
 mkdir 10m_120s_nvn_coexist
 while(( $int<=4 ))
 do
-    core_name=`expr $int \+ 2`
-    taskset -c $core_name nohup ./waf --run "scratch/laa --numA=$int --numB=$int --d2=10 --lbtTxop=5 --phyLogName=_phy_log_${int}v${int} --udpRate=750000000bps --duration=120 --logPhyArrivals=true" 2>&1  > ./10m_120s_nvn_coexist/${int}v${int} &
+    core_name=`expr $int \- 1`
+    taskset -c $core_name nohup ./waf --run "scratch/laa --numA=$int --numB=$int --d2=10 --lbtTxop=5 --phyLogName=_phy_log_coexist_${int}v${int} --udpRate=750000000bps --duration=120 --logPhyArrivals=true" 2>&1  > ./10m_120s_nvn_coexist/${int}v${int} &
     sleep 1
     let "int++"
 done
@@ -17,8 +17,8 @@ int=2
 mkdir 10m_120s_nvn_pure_wifi
 while(( $int<=4 ))
 do
-    core_name=`expr $int \+ 6`
-    taskset -c $core_name nohup ./waf --run "scratch/laa --cellConfigA=Wifi --numA=$int --numB=$int --d2=10 --lbtTxop=5 --phyLogName=_phy_log_${int}v${int} --udpRate=750000000bps --duration=120 --logPhyArrivals=true" 2>&1  > ./10m_120s_nvn_pure_wifi/${int}v${int} &
+    core_name=`expr $int \+ 2`
+    taskset -c $core_name nohup ./waf --run "scratch/laa --cellConfigA=Wifi --numA=$int --numB=$int --d2=10 --lbtTxop=5 --phyLogName=_phy_log_pure_wifi_${int}v${int} --udpRate=750000000bps --duration=120 --logPhyArrivals=true" 2>&1  > ./10m_120s_nvn_pure_wifi/${int}v${int} &
     sleep 1
     let "int++"
 done
